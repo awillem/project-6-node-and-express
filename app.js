af29b8f725919e50970145fa5314d6d1e38b97a5
Emma Willmann <emma.anne.81@gmail.com>
2018-09-30 21:03:55 -0500

    added images, changed css, updated readme

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.use('/images', express.static('images'));
 
 app.set('view engine', 'pug');
 
+// app.use((req, res, next) => {
+//     if(projects.length === 0) {
+        
+//     }
+// });
+
 app.get ('/', (req, res) => {
     
     res.render('index', { projects });
@@ -17,9 +23,30 @@ app.get ('/about', (req, res) => {
     res.render('about', {projects});
 });
 
+app.get('/projects', (req, res) => {
+    res.redirect('/');
+});
+
 app.get ('/projects/:id', (req, res) => {
     const {id} = req.params;
+    if( isNaN(id) || id >= projects.length) {
+      return  res.redirect('/');
+    }
     res.render('project', {id, projects});
+});
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    const message = "Sorry, that page is not found.";
+    console.log(message);
+    err.status = 404
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
 });
 
 app.listen(3000, () => {
